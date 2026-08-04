775f200f770263725bec8e07e7dc77907b04743c
mowen88 <mattowen88@outlook.com>
2026-08-04 17:14:54 +0100

    Refining door exits

--- states/world_state/entities/characters/player/player_states/player_exit_door.gd
+++ states/world_state/entities/characters/player/player_states/player_exit_door.gd
@@ -19,6 +19,9 @@ func physics_update(delta: float) -> void:
 	
 	timer -= delta
 	
+	# Add gravity
+	owner.velocity.y = min(owner.velocity.y + owner.move_component.gravity * delta,\
+	owner.move_component.max_fall_speed)
 	owner.move_and_slide()
 	
 	if timer <= 0:
